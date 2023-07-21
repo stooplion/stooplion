2a8b6d2b84eb27c3f9e29d0d3eac1a0820491bb2
stooplion <57123415+stooplion@users.noreply.github.com>
2023-07-20 18:24:25 -0700

    First successful deployment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
